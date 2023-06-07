8c53edd3b1f1be957acf823bf95bd7f18b531dcc
Eka Jacinda <rikkayuki68@gmail.com>
2023-06-07 11:16:26 +0700

    BISSMILAH BERHASI

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ResizeImage from "./ResizeImage";
 import ResizeAudio from "./ResizeAudio";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
      <>
      <BrowserRouter>
+     <Dashboard>
       <Routes>
         <Route path="/" element={<Dashboard/>}></Route>
         <Route path="/resizeImage" element={<ResizeImage/>}></Route>
         <Route path="/resizeAudio" element={<ResizeAudio/>}></Route>
       </Routes>
+      </Dashboard>
       </BrowserRouter>
      </>
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Pages/App';
-import Dashboard from './Pages/Dashboard';
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Dashboard/>
+    <App />
   </React.StrictMode>
 );
 
