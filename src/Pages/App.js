c2d525c17dfdb8765d9069cfe7bc1c6e761d6b6a
Eka Jacinda <rikkayuki68@gmail.com>
2023-06-07 10:53:23 +0700

    benerin app.js

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import ResizeImage from "./ResizeImage";
 import ResizeAudio from "./ResizeAudio";
 
 function App() {
   return (
-      <BrowserRouter>
-      <Routes>
+     <>
+      <Router>
+      <Dashboard></Dashboard>
+        <Switch>
         <Route path="/" element={<Dashboard/>}></Route>
         <Route path="/resizeImage" element={<ResizeImage/>}></Route>
         <Route path="/resizeAudio" element={<ResizeAudio/>}></Route>
-      </Routes>
-      </BrowserRouter>
+        </Switch>
+      </Router>
+     </>
 
   );
 }
